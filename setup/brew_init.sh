545c525b371f2c4ef7fd40c2b650e66d44ce9125
Saurabh <thes.kumar@gmail.com>
2013-09-29 02:25:22 +0530

    Add osx initial setup script

---
+++ setup/brew_init.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+# Upgrade any already-installed formulae
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+echo "Don’t forget to add $(brew --prefix coreutils)/libexec/gnubin to \$PATH."
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+# Install Bash 4
+brew install bash
+
+# Install wget with IRI support
+brew install wget --enable-iri
+
+# Install more recent versions of some OS X tools
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
+brew tap josegonzalez/homebrew-php
+
+# Install everything else
+brew install ack
+brew install git
+brew install rename
+brew install tree
+brew install legit # http://www.git-legit.org/
+brew install Zopfli # https://code.google.com/p/zopfli/
+
+# Remove outdated versions from the cellar
+brew cleanup
