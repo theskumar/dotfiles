feb1a1d49562476241027b1b902b76393c9029d6
Saurabh Kumar <thes.kumar@gmail.com>
2013-10-31 13:27:03 +0530

    brew: add check for brew installed or not.

--- setup/brew_init.sh
+++ setup/brew_init.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)" > /tmp/homebrew-install.log
+fi
+
 # Make sure we’re using the latest Homebrew
 brew update
 
@@ -36,3 +49,5 @@ git clone git://github.com/dronir/SpotifyControl.git ~/bin/SpotifyControl
 
 # Remove outdated versions from the cellar
 brew cleanup
+
+exit 0
