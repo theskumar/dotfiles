eaf753180b10b938af8fa75e69859ca2c50e947f
Saurabh Kumar <thes.kumar@gmail.com>
2013-11-02 12:38:42 +0530

    Add tweet script

--- setup/apt_init.sh
+++ setup/apt_init.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 install="apt-get install -y"
-declare -a packages=(fortune cowsay git git-extras)
+declare -a packages=(fortune cowsay git git-extras grc xclip)
 
 # http://github.com/ilikenwf/apt-fast
 # sudo git clone git@github.com:ilikenwf/apt-fast.git /usr/bin/apt-fast
