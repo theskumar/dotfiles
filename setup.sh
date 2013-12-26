950a3729752f341043d6a0df2cd4d32fb69abdc8
Saurabh Kumar <theskumar@Apples-iMac.local>
2013-12-26 21:25:27 +0530

    fix(submodule): clean up

--- setup.sh
+++ setup.sh
@@ -5,13 +5,13 @@
 
 function _setup() {
 
-    local setup_dir = '~/dotfiles/'
+    local setup_dir='~/dotfiles/'
     declare -a symoblic_links=(.ackrc .aliases .bashrc .curlrc .exports .exports .freebsd .functions .gitattributes .gitconfig .inputrc .linux .osx .profile .pythonrc.py .vimrc .wgetrc .xmobarrc .Xresources .zshrc .vim .xmonad)
 
     for file in ${symoblic_links[@]}
     do
         if [ -e $file ]; then
-            print $file
+            echo $file
         fi
     done
 }
