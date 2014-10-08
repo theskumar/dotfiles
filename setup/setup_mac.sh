2986e330ee8c96eab42a0e14c49f6e956d95d388
Saurabh Kumar <me+github@saurabh-kumar.com>
2014-10-08 08:34:11 +0530

    feat(mac): add nvault app

--- setup/setup_mac.sh
+++ setup/setup_mac.sh
@@ -63,10 +63,10 @@ function installcask() {
 }
 
 installcask google-chrome
-installcask adium
 installcask dropbox
 installcask iterm2
 installcask vlc
+installcask nvault  # for notes
 installcask qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webp-quicklook suspicious-package && qlmanage -r
 
 # Postgres 9 Database
@@ -89,6 +89,6 @@ brew cask install font-source-code-pro
 git clone git://github.com/dronir/SpotifyControl.git ~/bin/SpotifyControl
 
 # Remove outdated versions from the cellar
-brew cleanup
+brew cleanup && brew cask cleanup
 
 exit 0
