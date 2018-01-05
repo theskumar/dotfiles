0b8fa59db981c5a0ddbbe735038e6e123500d19e
Saurabh Kumar <saurabh@saurabh-kumar.com>
2018-01-05 12:18:37 +0530

    .pythonrc: add print_sql command

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,1 +1,13 @@
-import multiprocessing
+def print_sql(qs):
+    import sqlparse
+    try:
+        import pygments
+        from pygments.lexers import SqlLexer
+        from pygments.formatters import TerminalTrueColorFormatter
+    except ImportError:
+        pygments = None
+
+    sql = sqlparse.format(str(qs.query), reindent=True, keyword_case='upper')
+    if pygments:
+        sql = pygments.highlight(sql, SqlLexer(), TerminalTrueColorFormatter(style='monokai'))
+    print(sql)
